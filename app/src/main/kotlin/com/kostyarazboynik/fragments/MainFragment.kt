8f961bdf4b324401b7b2e8e5ce612f4296c5f3a8
Konstantin Kovalev <kvkovalev@avito.ru>
2026-03-29 15:52:36 +0300

    added navGraph and base fragments

---
+++ app/src/main/kotlin/com/kostyarazboynik/fragments/MainFragment.kt
@@ -0,0 +1,45 @@
+package com.kostyarazboynik.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [MainFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(view) {
+            findViewById<Button>(R.id.main_button).setOnClickListener {
+                findNavController().navigate(R.id.action_mainFragment_to_settingsFragment)
+            }
+        }
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @return A new instance of fragment MainFragment.
+         */
+        fun newInstance() = MainFragment()
+    }
+}
