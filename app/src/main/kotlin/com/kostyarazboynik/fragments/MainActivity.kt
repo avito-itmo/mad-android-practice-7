431de83a4cce84f9aaee31f55da7dc7ea7ef0446
Konstantin Kovalev <kvkovalev@avito.ru>
2026-03-29 17:51:06 +0300

    added bottom navigation

--- app/src/main/kotlin/com/kostyarazboynik/fragments/MainActivity.kt
+++ app/src/main/kotlin/com/kostyarazboynik/fragments/MainActivity.kt
@@ -2,10 +2,17 @@ package com.kostyarazboynik.fragments
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).setupWithNavController(
+            (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
+        )
     }
 }
