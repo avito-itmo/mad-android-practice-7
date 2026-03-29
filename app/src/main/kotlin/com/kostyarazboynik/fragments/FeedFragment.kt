7ca731427ee1a97f7bee0f75a880bb6e421eca8c
Konstantin Kovalev <kvkovalev@avito.ru>
2026-03-29 17:51:06 +0300

    added bottom navigation

---
+++ app/src/main/kotlin/com/kostyarazboynik/fragments/FeedFragment.kt
@@ -0,0 +1,33 @@
+package com.kostyarazboynik.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FeedFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FeedFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_feed, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @return A new instance of fragment FeedFragment.
+         */
+        fun newInstance() = FeedFragment()
+    }
+}
